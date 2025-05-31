60b16af3085a54e2e41462abe3aea474055c02af
Леонид Котов <leoska@MacBook-Pro-Leonid.local>
2025-05-31 18:58:36 +0300

    Init project

---
+++ eslint.config.mjs
@@ -0,0 +1,83 @@
+import path from 'node:path';
+
+import { includeIgnoreFile } from '@eslint/compat';
+import js from '@eslint/js';
+import { configs, plugins } from 'eslint-config-airbnb-extended';
+import { rules as prettierConfigRules } from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import jsonConfig from '@eslint/json';
+import markdown from '@eslint/markdown';
+
+export const projectRoot = path.resolve('.');
+export const gitignorePath = path.resolve(projectRoot, '.gitignore');
+
+const jsConfig = [
+  // ESLint Recommended Rules
+  {
+    name: 'js/config',
+    ...js.configs.recommended,
+  },
+  // Stylistic Plugin
+  plugins.stylistic,
+  // Import X Plugin
+  plugins.importX,
+  // Airbnb Base Recommended Config
+  ...configs.base.recommended,
+];
+
+const nodeConfig = [
+  // Node Plugin
+  plugins.node,
+  // Airbnb Node Recommended Config
+  ...configs.node.recommended,
+];
+
+const typescriptConfig = [
+  // TypeScript ESLint Plugin
+  plugins.typescriptEslint,
+  // Airbnb Base TypeScript Config
+  ...configs.base.typescript,
+];
+
+const prettierConfig = [
+  // Prettier Plugin
+  {
+    name: 'prettier/plugin/config',
+    plugins: {
+      prettier: prettierPlugin,
+    },
+  },
+  // Prettier Config
+  {
+    name: 'prettier/config',
+    rules: {
+      ...prettierConfigRules,
+      'prettier/prettier': 'error',
+    },
+  },
+];
+
+const mdConfig = [
+  {
+    files: ['**.md'],
+    plugins: { markdown },
+    processor: markdown.processors.markdown,
+  },
+];
+
+export default [
+  // Ignore .gitignore files/folder in eslint
+  includeIgnoreFile(gitignorePath),
+  // Javascript Config
+  ...jsConfig,
+  // Node Config
+  ...nodeConfig,
+  // TypeScript Config
+  ...typescriptConfig,
+  // Prettier Config
+  ...prettierConfig,
+  // JSON Config
+  ...[jsonConfig.configs.recommended],
+  // Markdown Config
+  ...mdConfig,
+];
